23748bb23bd6d9c430d2f055a656020beb760969
Jens Ochsenmeier <github@jens-ox.de>
2023-08-28 11:05:11 +0200

    let eslint run

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,28 +9,19 @@ import '@/styles/tailwind.css'
 const inter = Inter({
   subsets: ['latin'],
   display: 'swap',
-  variable: '--font-inter',
+  variable: '--font-inter'
 })
 
 export const metadata: Metadata = {
-  title:  'Kommunaler Fahrplan Open Data',
-  description:
-    'Eine Übersicht zu Strategie und Umsetzungsschritten für Open Data im kommunalen Bereich.',
+  title: 'Kommunaler Fahrplan Open Data',
+  description: 'Eine Übersicht zu Strategie und Umsetzungsschritten für Open Data im kommunalen Bereich.'
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html
-      lang="en"
-      className={clsx('h-full antialiased', inter.variable)}
-      suppressHydrationWarning
-    >
+    <html lang="en" className={clsx('h-full antialiased', inter.variable)} suppressHydrationWarning>
       <body className="flex min-h-full bg-white">
-          <Layout>{children}</Layout>
+        <Layout>{children}</Layout>
       </body>
     </html>
   )
